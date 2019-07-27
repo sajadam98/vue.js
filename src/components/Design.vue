e24d37c92653c1fdb0559ee1274e55f64adf5ea7
Sajad Amiri <sajad.vb98@gmail.com>
2019-07-27 02:36:47 -0700

    ui design

---
+++ src/components/Design.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="root">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <form>
+      <div class="text-input">
+        <label for="text_input" id="label-input"><b>Text :</b></label>
+        <textarea
+          type="text"
+          class="text-input-control"
+          id="txt-input"
+          aria-describedby="text-input"
+          placeholder="Enter Text"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">Translate</button>
+    </form>
+    <div class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+    <div>
+      <ul id="txt_translation">Translation Area</ul>
+    </div>
+  </div>
+</template>
+<style>
+.text-input {
+  width: 20%;
+  margin-left: 40%;
+  margin-right: 40%;
+}
+#label-input {
+  text-align: start;
+  display: block;
+  margin-top: 15px;
+}
+#txt-input {
+  width: 100%;
+  height: 150px;
+  resize: none;
+  font-size: 20px;
+}
+.btn {
+  margin-top: 20px;
+  width: 250px;
+  border-radius: 10px;
+}
+#txt_translation {
+  width: 30%;
+  margin-left: 35%;
+  margin-right: 35%;
+  font-size: 20px;
+  background-color: darkgray;
+  color: aliceblue;
+  font-family: Arial, Helvetica, sans-serif;
+}
+#root {
+  width: 100%;
+  height: 100%;
+  background-color: #fff8e1;
+}
+#about-us-title{
+  color: blue
+}
+#about-us-link{
+  color: blue;
+  font-size: 19px;
+  margin-left: 100px
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+      <a class="navbar-brand" href="#" style="margin-left: 25%;font-size: 28px; color: azure">دراک</a>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <div>
+              <a
+                class="nav-link"
+                id="about-us-link"
+                href="#"
+                @click="$bvModal.show('bv-modal-example')"
+              >
+                <b>درباره ما</b>
+                <span class="sr-only">(current)</span>
+              </a>
+
+              <b-modal id="bv-modal-example" hide-footer>
+                <template slot="modal-title">
+                  <h4 id="about-us-title">درباره ما</h4>
+                </template>
+                <div class="d-block text-center">
+                  <h5>
+                    شرکت هوشمندسازان دراک
+                    <br />شماره تماس : 09172338020
+                  </h5>
+                </div>
+                <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-example')">بستن</b-button>
+              </b-modal>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+</script>
+<style>
+</style>
